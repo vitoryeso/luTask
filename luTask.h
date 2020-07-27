0d1a82bc7f140bd228001b2d47a56fdf3e15c25f
vitoryeso <vitoryeso@outlook.com>
2020-07-26 21:34:33 -0300

    task class

---
+++ luTask.h
@@ -0,0 +1,36 @@
+#ifndef LU_TASK_H
+#define LU_TASK_H
+
+/*
+ * ok vamos criar um app de gerenciamento de tasks
+ * vamos poder criar listas e dentro dos quadros terao tasks
+ * o maior elemento visivel sao os quadros que contem as listas
+ * terao poucos quadros
+ *
+ */
+
+#include <string>
+#include "luUtils.h"
+
+using namespace std;
+
+class Task {
+private:
+    string content;
+    string initDate;
+    string finishDate;
+    bool done;
+public: 
+};
+
+class Quadro {
+
+private:
+
+
+};
+
+
+
+
+#endif
