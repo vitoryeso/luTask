40738a401d4cd215dd98b171036d1bd5f7417cdc
vitoryeso <vitoryeso@outlook.com>
2020-07-28 11:36:19 -0300

    list of taks

--- luTask.h
+++ luTask.h
@@ -10,48 +10,58 @@
  */
 
 #include <string>
-#include <list>
-#include "luUtils.h"
+#include <vector>
+#include <luUtils.h>
+
 
 using namespace std;
 
 class Task {
 private:
     string content;
-		string tag;
+	string tag;
     Date initDate;
     Date finishDate;
     bool done;
 public: 
-		Task(): content(""), tag(""), initDate.setCurrentDate(), done=false {}
-		Task(const string content, const string tag) {
-				this->content=content;
-				this->tag=tag;
-				this->initDate.setCurrentDate();
-				this->done=false;
-		}
-		inline string getContent() const {return this->content;}
-		inline string getTag() const {return this->tag;}
-		inline Date getInitDate() const {return this->initDate;}
-		inline Date getFinishDate() const {return this->finishDate;}
-		inline void setContent(const string content) {this->content=content;}
-		inline void setTag(const string tag) {this->tag=tag;}
+    // dont use
+	Task(): content(""), tag(""), done(false) {}
+    // use this
+	Task(const string content, const string tag) {
+		this->content=content;
+		this->tag=tag;
+		this->initDate.setCurrentDate();
+		this->done=false;
+	}
+	inline string getContent() const {return this->content;}
+	inline string getTag() const {return this->tag;}
+	inline Date getInitDate() const {return this->initDate;}
+	inline Date getFinishDate() const {return this->finishDate;}
+	inline void setContent(const string content) {this->content=content;}
+	inline void setTag(const string tag) {this->tag=tag;}
 
-		inline void finish() {finishDate.setCurrentDate(); done=true;}
+	inline void finish() {finishDate.setCurrentDate(); done=true;}
 };
 
+// this class get all tasks pending and finished. the idea is separate pending and finished using sort
 class TaskList {
 private:
-		string tag;
-		list<Task> tasks;
+	string name;
+	vector<Task> tasks;
 public:
-		TaskList(): tag("") {};
+	TaskList(): name("") {};
 
-		inline getTag(c
-		inline void addTask(const Task& T) {tasks.push_back(T);}		
-		inline void finishTask(const unsigned i) {tasks.at(i).finish();}
-		
-}
+    inline string getName() const {return this->name;}
+    inline Task getTask(const unsigned i) const {return this->tasks.at(i);}
+
+	inline void addTask(const Task& T) {tasks.push_back(T);}
+	inline void finishTask(const unsigned i) {tasks.at(i).finish();}
+    
+    void sortByDone();
+    unsigned getLastDone() const;
+    void sortByTag();
+    void sortByInitDate();    
+};
 
 class Quadro {
 private:
